ae4712329c33e4afd1f2bb12adc6cbe812e216e6
ASUS <ASUS@DESKTOP-15J4PS7>
2023-07-27 15:04:18 +0530

    Array changes

--- 11March_Evening_Batch/src/Array/Array_boolean.java
+++ 11March_Evening_Batch/src/Array/Array_boolean.java
@@ -11,7 +11,7 @@ public class Array_boolean {
 		B1[0] = true;
 		B1[1] = false;
 		B1[2] = true; // array initialization
-		
+		System.out.println("array boolean");
 		System.out.println(B1.length);
 		for(int i = 0; i<3; i++) {
 			System.out.println(B1[i]);
